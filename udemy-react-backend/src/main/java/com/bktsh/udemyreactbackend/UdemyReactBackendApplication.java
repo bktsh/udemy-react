84785dbac5121066def7faa760ef412e943dbda5
hashem <hashem.baktash@gmail.com>
2022-04-01 19:58:12 -0400

    added backend springboot project

---
+++ udemy-react-backend/src/main/java/com/bktsh/udemyreactbackend/UdemyReactBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bktsh.udemyreactbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyReactBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyReactBackendApplication.class, args);
+	}
+
+}
---
+++ udemy-react-backend/src/test/java/com/bktsh/udemyreactbackend/UdemyReactBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bktsh.udemyreactbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyReactBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
